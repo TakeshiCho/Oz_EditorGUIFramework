7c10044e7646afe8105f66528d5e37e6c8f5b8fd
Takeshi Chō <55079746+TakeshiCho@users.noreply.github.com>
2022-08-01 09:13:09 +0800

    Initialize

---
+++ OzEditorGUI/OzEditorGUI/OzComponent.cs
@@ -0,0 +1,40 @@
+﻿namespace OzEditorGUI
+{ 
+    public interface IOzInitor<out T>
+    {
+        T Init();
+    }
+
+    public interface IOzComponent
+    {
+        string GetName();
+        bool CannotOperate();
+        void Initialize();
+        void DrawGUI();
+        void DrawGizoms();
+        void Destroy();
+    }
+
+    public abstract class OzComponent<TClass> : IOzComponent where TClass : OzComponent<TClass>, new()
+    {
+        public class OzInitor<T> :IOzInitor<T> where T : OzComponent<T>,new()
+        {
+            public T Init()
+            {
+                T component = new T();
+                component.Initialize();
+                return component;
+            }
+        }
+        
+        public static OzInitor<TClass> initor = new OzInitor<TClass>();
+
+        public abstract void Initialize();
+        public abstract string GetName();
+        public abstract bool CannotOperate();
+        public abstract void DrawGUI();
+        public abstract void DrawGizoms();
+        public abstract void Destroy();
+        
+    }
+}
